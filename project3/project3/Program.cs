9a61a477604424c9ff7b6a0cf02a6236c8063a88
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-11-07 21:30:12 +0530

    project2

---
+++ project3/project3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project3
+{
+    internal class multiplication
+    {
+        static void Main(string[] args)
+        {
+            int a, b, r;
+            Console.WriteLine("enter first number here");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter sec number here");
+            b=Convert.ToInt32(Console.ReadLine());
+            r = (a*b);
+            Console.WriteLine("answer is" + r);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ project4/project4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project4
+{
+    internal class human
+    {
+        public static void Main(string[] args)
+        {
+            int x;
+            string personName;
+            int phoneNum;
+            Console.WriteLine("enter number");
+            x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter person name");
+            personName=Console.ReadLine();
+            Console.WriteLine("enter phone number");
+            phoneNum= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x={0}\n personName={1}\n phoneNum={2}",x,personName,phoneNum);
+            Console.ReadLine();
+        }
+    }
+}
