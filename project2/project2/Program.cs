7902887725dcb9f5717fd2cf6580e9f6b20b5d7f
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-11-07 21:26:31 +0530

    project2

---
+++ project2/project2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    internal class addition
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a+b);
+            Console.ReadLine();
+        }
+    }
+}
