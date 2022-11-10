0d08a4521b36299cd52125c8e831901fd01749fa
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-11-10 07:42:17 +0530

    UC1

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1
+{
+    internal class attendance
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("roll number\n" +random.Next(1,10));           
+            Console.ReadLine();     
+        }
+    }
+}
